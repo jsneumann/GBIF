ade3013b0120634afff5cc22f444afe6b5a37090
Johannes S. Neumann <johannes@amnh-its-MacBook-Pro.local>
2018-05-23 17:00:47 -0400

    rastR

--- R/rastR_downloadR.R
+++ R/rastR_downloadR.R
@@ -1,23 +1,24 @@
-#' Echo
-#' 
+#' rastR_downloadR
+#'
 #' This function:
 #' - reads in a list of GBIF taxonIDs
 #' - searches for the corresponding occurence maps using the map api
 #' - checks if there is no occurrence data and skips those urls to prevent abortion
 #' - writes .tif image files with the occurrence maps for each taxon into a new folder
-#' 
+#'
 #' @param webpage A list of URLs to download map data from
-#' @param ras All output file names will start with this, followed by taxonID
+#' @param rastR All output file names will start with this, followed by taxonID
 #'
 #' @export
 #' @examples
 #'
 
+# load necessary packages
 
 require(stringr)
 require(dismo)
 
-# read in data in form of a csv file containing the GBIF taxonIDs to search for 
+# read in data in form of a csv file containing the GBIF taxonIDs to search for
 
 taxonID_big = read.csv('~/Dropbox/Spatial_Bioinformatics/GBIF_Project/TaxonID_tiny.csv')
 taxonID_small = taxonID_big[,3]
@@ -30,8 +31,8 @@ webpage = paste0('https://api.gbif.org/v2/map/occurrence/density/0/0/0@1x.png?ta
 # The loop will produce an error if an output file of the same name exists (overwrite = FALSE)
 # Therefore, create a new folder to make sure it works
 
-dir.create('Raster')
-setwd('Raster')
+dir.create('rastR')
+setwd('rastR')
 
 # try(...) tests if the webpage provides an occurence map
 # class(...) assigns a new class "try-error", if there is an error for try()
@@ -42,9 +43,9 @@ setwd('Raster')
 stack1 = NULL
 
 for(i in 1:length(webpage)) {
-  if(class(try(raster(webpage[i]), silent = T)) == "try-error") {print(paste('could not find webpage', taxonID_small[i], sep=' '));
+  if(class(try(raster(webpage[i]), silent = T)) == "try-error") {print(paste('could not find webpage with taxonID', taxonID_small[i], sep=' '));
     next}
-  ra = raster(webpage[i])
-  # stack1 = stack(ra, stack1)
-  writeRaster(ra, paste('ras', taxonID_small[i], sep='_'), format = 'GTiff')
+  rastR = raster(webpage[i])
+  # stack1 = stack(rastR, stack1)
+  writeRaster(ra, paste('rastR_', taxonID_small[i], sep=''), format = 'GTiff')
 }
